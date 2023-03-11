34411b88fa88ad83d34db07686a217879ae07822
nominds <manish.thaper@gmail.com>
2023-03-11 22:22:42 +0530

    Pair2Test modified

--- CoreJavaPrac/src/com/java/core/generics/Pair2Test.java
+++ CoreJavaPrac/src/com/java/core/generics/Pair2Test.java
@@ -13,7 +13,27 @@ public class Pair2Test {
 				LocalDate.of(2022, 4, 22) //c
 			};	
 
-		
+		Pair<LocalDate> mm = ArrAlg.minmax(birthdays);
+		System.out.println("min = " + mm.getFirst());
+		System.out.println("max = " + mm.getSecond());
 	}
+	
 
+}
+
+class ArrAlg {
+	
+	public static <T extends Comparable> Pair <T> minmax(T[] a) {
+		if (a == null || a.length == 0) return null;
+		
+		T min = a[0];
+		T max = a[0];
+		
+		for (int i = 1; i < a.length; i++) {
+			if (min.compareTo(a[i]) > 0) min = a[i];
+			if (max.compareTo(a[i]) < 0) max = a[i];
+		}
+		
+		return new Pair<>(min,max);
+	}
 }
