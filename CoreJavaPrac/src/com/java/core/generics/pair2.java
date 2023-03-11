7f9400b12231cfdbeb718e205060c59dd6b6b1f0
nominds <manish.thaper@gmail.com>
2023-03-11 17:58:45 +0530

    Initial Commit

---
+++ CoreJavaPrac/src/com/java/core/generics/pair2.java
@@ -0,0 +1,19 @@
+package com.java.core.generics;
+
+import java.time.*;
+
+public class pair2 {
+
+	public static void main(String[] args) {
+
+		LocalDate [] birthdays = 
+			{
+				LocalDate.of(1958, 6, 7), //a
+				LocalDate.of(1979, 3, 20), // b
+				LocalDate.of(2022, 4, 22) //c
+			};	
+
+		
+	}
+
+}
---
+++ CoreJavaPrac/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sheru
+ *
+ */
+module CoreJavaPrac {
+}
